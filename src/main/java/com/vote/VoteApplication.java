5730cb3a0c94bf0199f6feb1bb544ba177bf8b4c
2008amin <52125684+2008amin@users.noreply.github.com>
2019-07-12 11:30:32 +0430

    Update VoteApplication.java

--- src/main/java/com/vote/VoteApplication.java
+++ src/main/java/com/vote/VoteApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class VoteApplication {
 
     public static void main(String[] args) {
+        //aaaa
         SpringApplication.run(VoteApplication.class, args);
     }
 }
